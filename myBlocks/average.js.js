325b401e79dcd1e9aedcb2baa62c7cea52a8a261
s17001 <s17001@std.it-college.ac.jp>
2019-07-23 11:41:57 +0900

    make code

--- myBlocks/average.js.js
+++ myBlocks/average.js.js
@@ -2,7 +2,14 @@ Blockly.JavaScript['average'] = function(block) {
   var value_v1 = Blockly.JavaScript.valueToCode(block, 'v1', Blockly.JavaScript.ORDER_ATOMIC);
   var value_v2 = Blockly.JavaScript.valueToCode(block, 'v2', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+  var code = '(' + value_v1 + '+' + value_v2 + ')/2';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['console_log'] = function(block) {
+  var value_msg = Blockly.JavaScript.valueToCode(block, 'msg', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'console.log(' + value_msg + ');';
+  return code;
 };
--- myBlocks/average_block.js
+++ myBlocks/average_block.js
@@ -1,16 +1,28 @@
 Blockly.Blocks['average'] = {
-  init: function() {
-    this.appendValueInput("v1")
-        .setCheck("Number");
-    this.appendValueInput("v2")
-        .setCheck("Number")
-        .appendField("と");
-    this.appendDummyInput()
-        .appendField("の平均値");
-    this.setInputsInline(true);
-    this.setOutput(true, null);
-    this.setColour(120);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
+    init: function() {
+        this.appendValueInput("v1")
+            .setCheck("Number");
+        this.appendValueInput("v2")
+            .setCheck("Number")
+            .appendField("と");
+        this.appendDummyInput()
+            .appendField("の平均値");
+        this.setInputsInline(true);
+        this.setOutput(true, null);
+        this.setColour(120);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['console_log'] = {
+    init: function() {
+        this.appendValueInput("msg")
+            .setCheck(null);
+        this.appendDummyInput()
+            .appendField("を出力する");
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
 };
