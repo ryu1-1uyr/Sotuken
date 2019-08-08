7fed546a4f6093dd9f015aa427ecb42ac91b3bbe
s17001 <s17001@std.it-college.ac.jp>
2019-08-08 16:19:36 +0900

    editorが正しいスペル…

---
+++ canvasSample/editor.js
@@ -0,0 +1,10 @@
+const editor = new Vue({
+    el: '#editor',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+    }
+})
