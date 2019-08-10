d3d3ce410c36ea400ee6c16d203c789dc7fb68ed
s17001 <s17001@std.it-college.ac.jp>
2019-08-10 22:28:50 +0900

    当然なんだけど、index.jsで定義した変数もmethods内から呼び出せた。密依存させれば速度出して開発はできると思う

--- canvasSample/editor.js
+++ canvasSample/editor.js
@@ -6,7 +6,24 @@ const editor = new Vue({
                 { id: 0, text: 'Vegetables' },
                 { id: 1, text: 'Cheese' },
                 { id: 2, text: 'Whatever else humans are supposed to eat' }
-            ]
+            ],
+            p1: {
+                name: 'sigeru',//これちゃんと自機だってわかる名前にしような
+                x: 0,
+                y: 400,
+                image: sigeru,
+                direction: 0,
+                HP: 100,
+                MP: 100,
+                commands: [],
+                isPlayer: true
+            }
         }
+    },
+    methods:{
+        checkP(){
+            console.log(this.p1)
+            console.log(p1)
+        },
     }
 })
