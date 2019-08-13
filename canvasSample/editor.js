2768946e646da88de94a042393e23db08b44f5be
s17001 <s17001@std.it-college.ac.jp>
2019-08-13 16:07:08 +0900

    ループの実装

--- canvasSample/editor.js
+++ canvasSample/editor.js
@@ -1,31 +1,45 @@
 const editor = new Vue({
     el: '#editor',
-    data(){
-        return{
+    // data(){
+    //     return{
+    //         groceryList: [
+    //             { id: 0, text: 'Vegetables' },
+    //             { id: 1, text: 'Cheese' },
+    //             { id: 2, text: 'Whatever else humans are supposed to eat' }
+    //         ],
+    //         loopCount: 0,
+    //         // p1: {
+    //         //     name: 'sigeru',//これちゃんと自機だってわかる名前にしような
+    //         //     x: 0,
+    //         //     y: 400,
+    //         //     image: sigeru,
+    //         //     direction: 0,
+    //         //     HP: 100,
+    //         //     MP: 100,
+    //         //     commands: [],
+    //         //     isPlayer: true
+    //         // }
+    //     }
+    // },
+    data: function() {
+        return {
             groceryList: [
                 { id: 0, text: 'Vegetables' },
                 { id: 1, text: 'Cheese' },
                 { id: 2, text: 'Whatever else humans are supposed to eat' }
             ],
-            // p1: {
-            //     name: 'sigeru',//これちゃんと自機だってわかる名前にしような
-            //     x: 0,
-            //     y: 400,
-            //     image: sigeru,
-            //     direction: 0,
-            //     HP: 100,
-            //     MP: 100,
-            //     commands: [],
-            //     isPlayer: true
-            // }
+            loopCount: 0,
         }
     },
     methods:{
+        test:()=>{
+            console.log(this.loopCount)
+            },
         addList: (value) => (commands) => {
             commands.push(value)
             console.log(commands,value)
         },
-        makeCode: selectedTarget => { // 絶対この関数は依存させてはならない
+        makeCode: function (selectedTarget) { // 絶対この関数は依存させてはならない
 
             console.log(selectedTarget)
 
@@ -74,8 +88,11 @@ const editor = new Vue({
                         stringCode += `switchTarget(attack)('${selectedTarget.name}');});`
                         break
                     case 'Lstart':
+                        stringCode += `for(let i=0;i<${this.loopCount};i=(i+1)|0){`
+                        console.log(this.loopCount)
                         break
                     case 'Lend':
+                        stringCode += '};'
                         break
                     default:
                         console.error('親に向かって何だその値は')
