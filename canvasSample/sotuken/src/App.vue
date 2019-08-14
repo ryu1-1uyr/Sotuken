824c4fed66bc578cd2533855155d9eeb3bbe98f4
s17001 <s17001@std.it-college.ac.jp>
2019-08-14 10:58:50 +0900

    値をそれっぽくした

--- canvasSample/sotuken/src/App.vue
+++ canvasSample/sotuken/src/App.vue
@@ -26,7 +26,7 @@
             <div class="col-3">
                 <h3>Draggable 1</h3>
                 <draggable class="dragArea list-group" :list="list1" :group="{ name: 'people', pull: 'clone', put: false }" @change="log">
-                    <div class="list-group-item" v-for="element in list1" :key="element.name">
+                    <div class="list-group-item" v-for="element in list1">
                         {{ element.name }}
                     </div>
                 </draggable>
@@ -35,7 +35,7 @@
             <div class="col-3">
                 <h3>Draggable 2</h3>
                 <draggable class="dragArea list-group" :list="list2" group="people" @change="log">
-                    <div class="list-group-item" v-for="element in list2" :key="element.name">
+                    <div class="list-group-item" v-for="element in list2" >
                         {{ element.name }}
                     </div>
                 </draggable>
@@ -60,14 +60,14 @@
             return {
                 loopCount: 0,
                 list1: [
-                    {name: "John", id: 1},
-                    {name: "Joao", id: 2},
-                    {name: "Jean", id: 3},
-                    {name: "Gerard", id: 4}
+                    {name: "1歩進む", id: 1},
+                    {name: "右を向く", id: 2},
+                    {name: "左を向く", id: 3},
+                    {name: "攻撃する", id: 4},
+                    {name: "繰り返し", id: 5},
+                    {name: "繰り返し終了", id: 6},
                 ],
                 list2: [
-                    {name: "Juan", id: 5},
-                    {name: "Edgard", id: 6},
                     {name: "Johnson", id: 7}
                 ],
             }
