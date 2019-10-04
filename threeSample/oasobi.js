5c3139c253531819203a27064e4cd68213e5c7fa
s17001 <s17001@std.it-college.ac.jp>
2019-10-04 15:55:39 +0900

    add oasobi functions

---
+++ threeSample/oasobi.js
@@ -0,0 +1,58 @@
+const scale = () => {
+    myShape.scale.x = 5
+    myShape.scale.y = 10
+    myShape.scale.z = 15
+}
+
+const scale2 = () => {
+    myShape.scale.x = 1
+    myShape.scale.y = 1
+    myShape.scale.z = 1
+}
+
+const gucha =() => {
+    myShape.rotation.x += 1;
+    myShape.rotation.z += 1;
+}
+
+const scaleALL = () => {
+    myShape.scale.x = 10
+    myShape.scale.y = 10
+    myShape.scale.z = 10
+
+    myShape2.scale.x = 10
+    myShape2.scale.y = 10
+    myShape2.scale.z = 10
+
+    myShape3.scale.x = 10
+    myShape3.scale.y = 10
+    myShape3.scale.z = 10
+
+    myShape4.scale.x = 10
+    myShape4.scale.y = 10
+    myShape4.scale.z = 10
+
+
+}
+
+const scale2ALL = () => {
+    myShape.scale.x = 1
+    myShape.scale.y = 1
+    myShape.scale.z = 1
+
+    myShape2.scale.x = 1
+    myShape2.scale.y = 1
+    myShape2.scale.z = 1
+
+    myShape3.scale.x = 1
+    myShape3.scale.y = 1
+    myShape3.scale.z = 1
+
+    myShape4.scale.x = 1
+    myShape4.scale.y = 1
+    myShape4.scale.z = 1
+}
+
+const rotate2 = () => {
+    myShape.rotation.y += 0.5;
+}
