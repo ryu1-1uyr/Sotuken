842cb95032de6ea8cdec9af5698f4239712deaab
s17001 <s17001@std.it-college.ac.jp>
2019-10-30 15:37:04 +0900

    add battle page

---
+++ hubpage/pages/list/_id/battle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>ここでバトルするようなページを作りたいね</p>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "battle"
+    }
+</script>
+
+<style scoped>
+
+</style>
