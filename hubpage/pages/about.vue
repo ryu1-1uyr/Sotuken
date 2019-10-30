8e83d5fd0833e13915dae3e306312810d180b376
s17001 <s17001@std.it-college.ac.jp>
2019-10-30 15:31:48 +0900

    add list about

---
+++ hubpage/pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      ここではあばうとみ〜について色々書いていきます
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "about"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ hubpage/pages/list/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div>
+      <div class="container">
+        <div v-for="i in datalist" class="cardElement">
+          <b-card
+            :title="'My battle AI for '+ i "
+            img-src="~/assets/sumpleShoot.png"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              This is my team at Craftomy . This is my team at Craftomy . This is my team at Craftomy . This is my team
+              at Craftomy .
+            </b-card-text>
+
+            <b-button href="#" class="mybutton" variant="outline">Battle!!</b-button>
+          </b-card>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "index",
+    data() {
+      return {
+        datalist: [...Array(31).keys()]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .container {
+    padding: 30px;
+    display: flex;
+    flex-wrap: wrap;
+
+    justify-content: center;
+    align-items: center;
+
+  }
+
+  .cardElement {
+    margin: 20px;
+  }
+
+  .mybutton {
+    background: rgba(161, 214, 226, 0.76);
+  }
+</style>
