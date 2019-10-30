50f8101119ff0f7308de4d2d18a109467abb5708
s17001 <s17001@std.it-college.ac.jp>
2019-10-30 14:37:10 +0900

    headerをcompornentsか

---
+++ hubpage/components/header.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+<!--    <p>hogehoge!!!!!!</p>-->
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+<!--          <b-nav-form>-->
+<!--            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>-->
+<!--            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>-->
+<!--          </b-nav-form>-->
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">JA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="/mypage">Profile</b-dropdown-item>
+
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+<!--            認証あたりと組み合わせてlogin,logout分ける奴やる-->
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "header"
+    }
+</script>
+
+<style scoped>
+
+</style>
