b35868a94ef7253a5717f9631c079590f7333db4
s17001 <s17001@std.it-college.ac.jp>
2019-10-30 14:37:59 +0900

    雑多なコミット

--- hubpage/pages/mypage.vue
+++ hubpage/pages/mypage.vue
@@ -1,13 +1,13 @@
 <template>
   <el-container>
-    <h1>こんにちは、{{this.$auth.$state.user.family_name}}さん!</h1>
+    <h1>こんにちは、{{this.$auth.$state.user.name}}さん!</h1>
   </el-container>
 </template>
 
 <script>
-    export default {
-        name: "mypage"
-    }
+  export default {
+    name: "mypage",
+  }
 </script>
 
 <style scoped>
