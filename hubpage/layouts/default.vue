1fd51f95fd7606a02eeb933fedae73a65f527a13
s17001 <s17001@std.it-college.ac.jp>
2019-10-30 14:37:24 +0900

    layout fix

--- hubpage/layouts/default.vue
+++ hubpage/layouts/default.vue
@@ -1,72 +1,93 @@
 <template>
   <div>
-      <div>
-        <div class="line"></div>
-        <v-navbar
-          mode="horizontal"
-          background-color="#545c64"
-          text-color="#fff"
-          active-text-color="#ffd04b">
-          <b-nav-item><nuxt-link to="/">Home</nuxt-link></b-nav-item>
-          <b-nav-item><nuxt-link to="/mypage">Mypage</nuxt-link></b-nav-item>
-          <b-nav-item><nuxt-link to="/login">Login</nuxt-link></b-nav-item>
-        </v-navbar>
-        <b-card>
-          <el-container fluid fill-height>
-            <nuxt/>
-          </el-container>
-        </b-card>
-      </div>
+    <Header></Header>
+<!--    <div>-->
+<!--      <div class="line"></div>-->
+<!--      <v-navbar-->
+<!--        mode="horizontal"-->
+<!--        background-color="#545c64"-->
+<!--        text-color="#fff"-->
+<!--        active-text-color="#ffd04b">-->
+<!--        <b-nav-item>-->
+<!--          <nuxt-link to="/">Home</nuxt-link>-->
+<!--        </b-nav-item>-->
+<!--        <b-nav-item>-->
+<!--          <nuxt-link to="/mypage">Mypage</nuxt-link>-->
+<!--        </b-nav-item>-->
+<!--        <b-nav-item>-->
+<!--          <nuxt-link to="/login">Login</nuxt-link>-->
+<!--        </b-nav-item>-->
+<!--        <p @click="loginWithAuthZero">Login</p>-->
+<!--        <p @click="logout">Logout</p>-->
+<!--      </v-navbar>-->
+<!--      <b-card>-->
+<!--        <el-container fluid fill-height>-->
+<!--          <nuxt/>-->
+<!--        </el-container>-->
+<!--      </b-card>-->
+<!--    </div>-->
 
   </div>
 </template>
+<script>
+  import Header from '~/components/header.vue'
+  // import Header from '~/components/Logo.vue'
+
+
+  export default {
+    components:{
+      Header,
+      // Logo
+    }
+  }
+</script>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+  html {
+    font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+    font-size: 16px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+    margin: 0;
+  }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+  .button--green {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #3b8070;
+    color: #3b8070;
+    text-decoration: none;
+    padding: 10px 30px;
+  }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
+  .button--green:hover {
+    color: #fff;
+    background-color: #3b8070;
+  }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
+  .button--grey {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #35495e;
+    color: #35495e;
+    text-decoration: none;
+    padding: 10px 30px;
+    margin-left: 15px;
+  }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+  .button--grey:hover {
+    color: #fff;
+    background-color: #35495e;
+  }
 </style>
