ebaec930c6a1c5a176d1eba1734832b3452b66ca
s17001 <s17001@std.it-college.ac.jp>
2019-10-30 17:05:36 +0900

    背景にビデオを使った

--- hubpage/pages/index.vue
+++ hubpage/pages/index.vue
@@ -1,14 +1,23 @@
 <template>
   <section class="container">
+    <div id="bg-video-cover"></div>
+    <video id="bg-video" src="~/assets/sample.mp4" autoplay="autoplay" muted loop></video>
+
     <div>
-      <logo />
-      <h1 class="title">
-        hubpage
-      </h1>
-      <h2 class="subtitle">
-        My grand Nuxt.js project
-      </h2>
-      <div class="links">
+
+      <div class="medatu">
+        <img src="~/assets/craftomy.svg"/>
+        <h1 class="title">
+          Craftomy
+        </h1>
+        <h2 class="subtitle">
+          My grand Nuxt.js project
+        </h2>
+      </div>
+
+
+
+      <div class="links medatu">
         <a
           href="https://nuxtjs.org/"
           target="_blank"
@@ -25,44 +34,74 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+  import Logo from '~/components/Logo.vue'
 
-export default {
-  components: {
-    Logo
+  export default {
+    components: {
+      Logo
+    }
   }
-}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .links {
+    padding: 15px;
+  }
+
+  #bg-video {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    z-index: -2;
+    background-size: cover;
+  }
+
+  #bg-video-cover {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    z-index: -1;
+    background-size: cover;
+    background: rgba(0, 0, 0, 0.54);
+  }
+
+  .medatu {
+    background: rgba(241,241,242,0.75);
+    border-radius: 20px;
+  }
 </style>
