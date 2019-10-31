00ce34880ad2cb77545363d9b8ddb45598c63bdd
s17001 <s17001@std.it-college.ac.jp>
2019-10-31 16:13:35 +0900

    これは作り直さないといけにあ

--- hubpage/components/screen.vue
+++ hubpage/components/screen.vue
@@ -1,14 +1,30 @@
 <template>
-    <div>
-      すくりーんだよ〜ん
-    </div>
+  <div>
+    <canvas id='myCanvas'></canvas>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "screen"
+  export default {
+    name: "screen",
+    head: {
+      script: [
+        {src: 'https://cdn.jsdelivr.net/npm/three@0.110.0/build/three.min.js'},
+        {src: 'https://cdn.jsdelivr.net/npm/three-orbitcontrols@2.108.1/OrbitControls.min.js'},
+        // {src: '@/assets/index.js'},
+        // {src: '../assets/myFunctionDefinition.js'},
+      ],
+      link: [
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto&display=swap'}
+      ]
     }
+  }
+
+
 </script>
+
+<!--<script src="~/assets/three.js"></script>-->
+<!--<script src="~/assets/index.js"></script>-->
 
 <style scoped>
 
