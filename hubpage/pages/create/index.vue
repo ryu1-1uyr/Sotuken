46f121d86850b641fab5cec07425eba5d9cb1a32
s17001 <s17001@std.it-college.ac.jp>
2019-10-31 15:25:22 +0900

    fix style

--- hubpage/pages/create/index.vue
+++ hubpage/pages/create/index.vue
@@ -1,21 +1,38 @@
 <template>
   <div>
-    <editor />
+    <div class="container">
+      <screen />
+      <editor />
+    </div>
   </div>
 
 </template>
 
 <script>
   import editor from '~/components/editor.vue'
+  import screen from '~/components/screen.vue'
+
 
   export default {
     components: {
-      editor
+      editor,
+      screen
     },
     name: "index"
   }
 </script>
 
 <style scoped>
+  .container {
+    /*padding: 30px;*/
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
 
+    /*justify-content: center;*/
+    /*align-items: center;*/
+    align-items: flex-start;
+
+
+  }
 </style>
